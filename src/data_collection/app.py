76e2da94a40b921d21a8bf594a4bf8ff67ae253e
hitansh1299 <hitansh1299@gmail.com>
2023-09-19 23:05:10 +0530

    added feature to download files and view all files

--- src/data_collection/app.py
+++ src/data_collection/app.py
@@ -1,6 +1,6 @@
 #create a basic flask app
 import os
-from flask import Flask, render_template, redirect, flash, request, url_for, send_from_directory
+from flask import Flask, render_template, redirect, flash, request, url_for, send_from_directory, jsonify
 from werkzeug.utils import secure_filename
 from datetime import datetime
 UPLOAD_FOLDER = 'C:/Users/hitan/OneDrive/Desktop/MiniProjects/IITB_LCS/src/data_collection/uploads'
@@ -13,12 +13,6 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.add_url_rule(
     "/uploads/<name>", endpoint="download_file", build_only=True
 )
-
-
-# @app.route('/')
-# def home():
-#     pass
-
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -48,14 +42,23 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = datetime.now().strftime("%Y%m%d_%H%M%S") + "_" + request.form['location']+ "_" + request.form['type'] + "_" + secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            process_file(filename=filename)
             print('File saved successfully')
             # return redirect(url_for('download_file', name=filename))
     return render_template('index.html')
 
-@app.route('/uploads/<name>')
+@app.route('/download_file/<name>')
 def download_file(name):
     return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
+@app.route('/listfiles', methods=['GET'])
+def list_files():
+    data = os.listdir(app.config["UPLOAD_FOLDER"])
+    print(data)
+    return jsonify(data)
+
+def process_file(filename: str):
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
